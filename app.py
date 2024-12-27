7599264885a1040332ceeeb18182a274a7877181
valevil <swamptk@gmail.com>
2024-12-27 23:11:41 +0100

    ADD: shiny app structure

---
+++ app.py
@@ -0,0 +1,18 @@
+import seaborn as sns
+
+# Import data from shared.py
+from shared import df
+from shiny.express import input, render, ui
+
+# Page title (with some additional top padding)
+ui.page_opts(title="Hello there")
+
+# Render a histogram of the selected variable (input.var())
+@render.plot
+def hist():
+    p = sns.histplot(df, x=input.var(), facecolor="#007bc2", edgecolor="white")
+    return p.set(xlabel=None)
+
+
+# Select input for choosing the variable to plot
+ui.input_select("var", "Select variable", choices=["bill_length_mm", "body_mass_g"])
