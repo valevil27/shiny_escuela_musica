d7f518c586418c63f23f7020f54343698a5244ba
valevil <swamptk@gmail.com>
2024-12-28 21:09:26 +0100

    ADD - trimestre y curso actuales en titulo

--- app.py
+++ app.py
@@ -2,27 +2,40 @@ from datetime import date
 import seaborn as sns
 
 # Import data from shared.py
-from shared import df, start_time, filter_options
+from shared import df, filter_options, last_entry_ds
 from shiny.express import input, render, ui
 import locale
 
 # Idioma local castellano para trabajar con strftime y fechas, _ para no renderizar
-_ = locale.setlocale(locale.LC_TIME, '')
+_ = locale.setlocale(locale.LC_TIME, "")
+actual_trim, actual_course = last_entry_ds(date.today())
+
 
 # Barra de título
 @render.express
 def render_title():
-    with ui.layout_columns(class_ = "mt-3 "):
-        ui.h3(f"Escuela de Música - {input.filter()}", class_ = "text-lg")
-        ui.h6("Fecha de consulta: " + date.today().strftime("%d de %B de %Y"), class_ = "mb-0", style = "line-height: 2.5;")
+    with ui.layout_columns(class_="mt-3 "):
+        ui.h2(f"Escuela de Música - {input.filter()}")
+        ui.div(
+            ui.p(
+                "Fecha de consulta: " + date.today().strftime("%d de %B de %Y"),
+                class_="mb-0",
+                style="font-size: small;",
+            ),
+            ui.p(
+                f"Trimestre {actual_trim}, Curso {actual_course}",
+                style="font-size: small;",
+            ),
+        )
 
 
 # Filtros del Dashboard
 with ui.sidebar():
     ui.h4("Filtros")
-    ui.input_date("fecha_inicio", "Fecha de inicio", value = start_time, language="es", format = "mm-yyyy")
-
-    ui.input_selectize("filter", "Aspecto a Examinar", choices=filter_options, selected="General")
+    ui.input_date("fecha_inicio", "Fecha de inicio", language="es", format="mm-yyyy")
+    ui.input_selectize(
+        "filter", "Aspecto a Examinar", choices=filter_options, selected="General"
+    )
 
 # Fila de bars
 with ui.layout_columns():
@@ -34,5 +47,3 @@ with ui.layout_columns():
 
     with ui.card():
         ui.card_header("Promedio de asistencia")
-    
-
