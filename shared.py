d14e3c7a416d374a1396b54ee5fd0b6b02dc5f65
valevil <swamptk@gmail.com>
2024-12-28 20:35:32 +0100

    ADD - Opciones filtros, import del df, tiempo

--- shared.py
+++ shared.py
@@ -1,4 +1,15 @@
+from datetime import date, timedelta
 from pathlib import Path
 import pandas as pd
 
+app_dir = Path(__file__).parent
+df = pd.read_csv(app_dir / "dataset.csv")
+start_time = date.today() - timedelta(weeks=12)
 
+filter_options = [
+    "General",
+    "Curso",
+    "Asignatura",
+    "Profesor",
+    "Instrumento",
+]
